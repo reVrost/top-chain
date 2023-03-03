202f6f70389ce64b6e45bae03a2242d6b485eeb6
yundifu <yundi.fu77@gmail.com>
2023-03-03 14:11:25 +1100

    change color

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -57,9 +57,11 @@ contract MyERC721 is ImmutableERC721Preset {
 
     function _renderFromSeed(uint256 burntWei) public returns(string memory){
         bytes memory svg = abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
-            '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
-            '<rect width="100%" height="100%" fill="black" />',
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 200 200">',
+            '<defs> <linearGradient id="Gradient1"> <stop class="stop1" offset="0%" /> <stop class="stop2" offset="50%" /> <stop class="stop3" offset="100%" /> </linearGradient>',
+            '<linearGradient id="Gradient2" x1="0" x2="0" y1="0" y2="1"><stop offset="0%" stop-color="pink" /><stop offset="50%" stop-color="black" stop-opacity="0" /> <stop offset="100%" stop-color="blue" /></linearGradient>',
+            '<style> <![CDATA[#rect1 { fill: url(#Gradient1); }.stop1 { stop-color: pink; }.stop2 { stop-color: black; stop-opacity: 0; }.stop3 { stop-color: blue; }]]> </style></defs>',
+             '<rect width="200px" height="200px" fill="url(#Gradient2)"/>',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Welcome Top Chainer",'</text>',
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "You burnt: ",Strings.toString(burntWei),'wei</text>',
             '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">',"WHAT A HERO",'</text>',
